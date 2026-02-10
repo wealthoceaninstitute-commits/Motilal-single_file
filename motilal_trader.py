40e8e4e2b54ec07bc66c7210bc268a51437d532b
wealthoceaninstitute-commits <wealthoceaninstitute@gmail.com>
2026-02-10 20:59:05 +0530

    Update motilal_trader.py

--- motilal_trader.py
+++ motilal_trader.py
@@ -632,7 +632,7 @@ async def edit_client(request: Request, payload: dict = Body(...)):
 
     return {"success": True}
 
-@app.get("/clients")
+@app.get("/get_clients")
 def get_clients(request: Request):
 
     owner_userid = request.headers.get("x-user-id") \
